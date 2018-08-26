ba42e0d820b68483af82e10e56dc072906d951e9
nomand <alexey.botkov@gmail.com>
2018-08-26 22:45:43 +1200

    t01 bytes

---
+++ Assets/Runningtap/Scenes/Test01/Test01Agent2.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class Test01Agent2 : Agent
+{
+    public Transform Ball;
+    public Transform Set;
+    public BoxCollider inputBound;
+
+    public float speed = 3f;
+    public float turnSpeed = 5f;
+    Rigidbody rb;
+    Rigidbody ballrb;
+    RayPerception rp;
+    Bounds bound;
+
+    float rotation;
+    float action1;
+    Vector3 newHeading;
+
+    Material mat;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        ballrb = Ball.GetComponent<Rigidbody>();
+        rp = GetComponent<RayPerception>();
+        bound = inputBound.bounds;
+        mat = Set.GetComponent<Renderer>().material;
+    }
+
+    public override void AgentReset()
+    {
+        transform.position = new Vector3(Set.transform.position.x - Random.Range(-9.5f, 9.5f), 0.5f, Set.transform.position.z - Random.Range(-9.5f, 9.5f));
+        Ball.transform.position = new Vector3(Set.transform.position.x - Random.Range(-9.5f, 9.5f), 0.5f, Set.transform.position.z - Random.Range(-9.5f, 9.5f));
+
+        transform.Rotate(new Vector3(0, Random.Range(-180, 180), 0));
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        ballrb.velocity = Vector3.zero;
+        ballrb.angularVelocity = Vector3.zero;
+    }
+
+    bool BoundsContain(Transform t)
+    {
+        if(bound.Contains(t.position))
+            return true;
+        else
+            return false;
+    }
+
+    public override void CollectObservations()
+    {
+        var rayDistance = 10f;
+        float[] rayAngles = { 90f };
+        var detectableObjects = new[] { "ball", "wall" };
+        AddVectorObs(rp.Perceive(rayDistance, rayAngles, detectableObjects, 0f, 0f));
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        //reward if you get the ball out of bounds
+        if (!BoundsContain(Ball))
+        {
+            AddReward(1f);
+            StartCoroutine(Success(Color.green));
+            Done();
+        }
+
+        //penalty if you go out of bounds
+        if(!BoundsContain(transform))
+        {
+            AddReward(-1f);
+            StartCoroutine(Success(Color.red));
+            AgentReset();
+        }
+
+        //hurry up
+        AddReward(-1f / agentParameters.maxStep);
+
+        rotation += vectorAction[0] * turnSpeed;
+        action1 = vectorAction[1];
+    }
+
+    void FixedUpdate()
+    {
+        newHeading = new Vector3(0f, rotation, 0f);
+        rb.MoveRotation(Quaternion.Euler(newHeading));
+        rb.AddForce(transform.forward * action1 * speed, ForceMode.VelocityChange);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == Ball.gameObject)
+        {
+            AddReward(collision.relativeVelocity.magnitude);
+        }
+    }
+
+    IEnumerator Success(Color c)
+    {
+        mat.color = c;
+        yield return new WaitForSeconds(1);
+        mat.color = Color.grey;
+        yield return null;
+    }
+}
